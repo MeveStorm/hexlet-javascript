9d5f3e23c3952198233669f1e6a7cd396783df2a
MeveStorm <44941747+MeveStorm@users.noreply.github.com>
2022-09-24 20:06:47 +0300

    fix a solution to the exercise topic "4_checking_value"

--- src/arrays/4_checking_value/arrays.js
+++ src/arrays/4_checking_value/arrays.js
@@ -1,8 +1,8 @@
 const getValueByIndex = (array, index, defaultValue = null) => {
-  const length = array.length - 1;
+  const lastIndex = array.length - 1;
   const isPositive = index >= 0;
 
-  return index <= length && isPositive ? array[index] : defaultValue;
+  return index <= lastIndex && isPositive ? array[index] : defaultValue;
 };
 
 export default getValueByIndex;
