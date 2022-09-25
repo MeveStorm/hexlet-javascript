d7ffb6dbe31f764537ddb8f38ebd441710c37c37
MeveStorm <44941747+MeveStorm@users.noreply.github.com>
2022-09-26 01:06:34 +0300

    refactor a solution to the exercise topic "7_aggregation"

--- src/arrays/7_aggregation/arrays.js
+++ src/arrays/7_aggregation/arrays.js
@@ -1,8 +1,8 @@
 /* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
-const calculateSum = (arrays) => {
+const calculateSum = (array) => {
   let sum = 0;
 
-  for (const number of arrays) {
+  for (const number of array) {
     if (number % 3 === 0) {
       sum += number;
     }
