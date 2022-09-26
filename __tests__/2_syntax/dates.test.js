32f121461fffd6967cb7f3a260971819274bb2a0
MeveStorm <44941747+MeveStorm@users.noreply.github.com>
2022-09-26 12:05:28 +0300

    added tests

---
+++ __tests__/2_syntax/dates.test.js
@@ -0,0 +1,7 @@
+import getWeekends from '../../src/arrays/2_syntax/dates';
+
+test('get', () => {
+  expect(getWeekends()).toEqual(['saturday', 'sunday']);
+  expect(getWeekends('long')).toEqual(['saturday', 'sunday']);
+  expect(getWeekends('short')).toEqual(['sat', 'sun']);
+});
