74dd9f49f6c240ebc1286ba7b70a861e197f1779
MeveStorm <44941747+MeveStorm@users.noreply.github.com>
2022-10-06 12:28:57 +0300

    added a solution to the exercise topic "20_rest_operator"

--- __tests__/arrays_tests/20_rest_operator/arrays.test.js
+++ __tests__/arrays_tests/20_rest_operator/arrays.test.js
@@ -1,11 +1,8 @@
-it('', () => {
-});
+import { getMax } from '../../../src/arrays/20_rest_operator/arrays';
 
-//
-//
-// test('max', () => {
-//   expect(getMax([])).toEqual(null);
-//   expect(getMax([1, 10, 8])).toEqual(10);
-//   expect(getMax([11, -3, 8, 1])).toEqual(11);
-//   expect(getMax([1, 8, -3, 11])).toEqual(11);
-// });
+test('max', () => {
+  expect(getMax([])).toEqual(null);
+  expect(getMax([1, 10, 8])).toEqual(10);
+  expect(getMax([11, -3, 8, 1])).toEqual(11);
+  expect(getMax([1, 8, -3, 11])).toEqual(11);
+});
