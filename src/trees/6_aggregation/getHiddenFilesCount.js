0ecec2140ff3340c83d727d4b6c249588f058ebe
MeveStorm <44941747+MeveStorm@users.noreply.github.com>
2023-09-19 11:39:57 +0300

    added a solution to the exercise topic 'trees/6_aggregation'

---
+++ src/trees/6_aggregation/getHiddenFilesCount.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import { isFile, getName, getChildren } from '@hexlet/immutable-fs-trees';
+
+const getHiddenFilesCount = (tree) => {
+  if (isFile(tree)) {
+    console.log(getName(tree));
+    return getName(tree).startsWith('.') ? 1 : 0;
+  }
+
+  const children = getChildren(tree);
+  const hiddenCounts = children.map(getHiddenFilesCount);
+  return _.sum(hiddenCounts);
+};
+
+export default getHiddenFilesCount;
